a49c751e358a46231c2b0f2b8e6b4ea1bb247f6a
wasted4k <vfreiber@gmail.com>
2025-10-07 01:19:12 -0500

    Update support_chat.php

--- api/support_chat.php
+++ api/support_chat.php
@@ -105,7 +105,7 @@ try {
   $method = $_SERVER['REQUEST_METHOD'] ?? 'GET';
   $action = $_GET['action'] ?? '';
 
-  if ($method == 'GET' || $method == 'POST') {
+  if ($method == 'GET' && $action == 'thread') {
     
     // trae (o crea) ticket del usuario
     $ticket = ensureUserTicket($pdo, (int)$user_id);
