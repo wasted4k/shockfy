ea2f4de82f5c1b548d84b98d77c94551dcaa3cfa
wasted4k <vfreiber@gmail.com>
2025-10-07 01:39:19 -0500

    Update support_chat.php

--- api/support_chat.php
+++ api/support_chat.php
@@ -164,10 +164,10 @@ try {
 
     // ticket y mensaje
     $ticket = ensureUserTicket($pdo, (int)$user_id);
-    echo $message;
-    die();
     $pdo->beginTransaction();
     $pdo->exec("SET SESSION time_zone = '+00:00'");
+    echo $message;
+    die();
     $ins = $pdo->prepare("INSERT INTO support_messages (ticket_id, sender, message, file_path, created_at) VALUES (?, 'user', ?, ?, NOW())");
     $ins->execute([(int)$ticket['id'], $message ?: null, $filePath]);
 
